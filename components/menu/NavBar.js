f2c3ecdb3a5646d881b3ebffee34f2f7b5b94fa0
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2020-09-22 17:17:01 -0600

    Allow opening General Menu only after logged in. Display an alert otherwise.

--- components/menu/NavBar.js
+++ components/menu/NavBar.js
@@ -1,12 +1,25 @@
 import * as React from 'react';
-import {Button} from 'react-native';
+import {Alert, Button} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import * as Navigation from './RootNavigation';
+import {useUserPermissions} from '../hooks/UserPermissionsProvider';
 
 const NavBar = () => {
+  const isSomebodyLoggedIn = useUserPermissions()[0].loggedIn;
   return (
     <SafeAreaView>
-      <Button title="Open Menu" onPress={() => Navigation.toggleDrawer()} />
+      <Button
+        title="Open Menu"
+        onPress={() => {
+          if (isSomebodyLoggedIn) {
+            Navigation.toggleDrawer();
+          } else {
+            Alert.alert('Message', 'Please Log in First', [{text: 'OK'}], {
+              cancelable: true,
+            });
+          }
+        }}
+      />
     </SafeAreaView>
   );
 };
