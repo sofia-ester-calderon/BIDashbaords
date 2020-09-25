67cd83b18a25c8b741f0fc75aa1c09a863a5aafb
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2020-09-25 16:07:04 -0600

    Use TouchableOpacity instead of Button. Add Image.

--- components/menu/NavBar.js
+++ components/menu/NavBar.js
@@ -1,27 +1,39 @@
 import React from 'react';
-import {Alert, Button} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {Alert, Button, Image, Text, TouchableOpacity, View} from 'react-native';
 import * as Navigation from './RootNavigation';
 import {useUserPermissions} from '../hooks/UserPermissionsProvider';
 
 const NavBar = () => {
   const [userPermissions] = useUserPermissions();
 
+  const handleOnPress = () => {
+    if (userPermissions.loggedIn) {
+      Navigation.toggleDrawer();
+    } else {
+      Alert.alert('Message', 'Please Log in First!', [{text: 'OK'}], {
+        cancelable: true,
+      });
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Button
-        title="Open Menu"
-        onPress={() => {
-          if (userPermissions.loggedIn) {
-            Navigation.toggleDrawer();
-          } else {
-            Alert.alert('Message', 'Please Log in First', [{text: 'OK'}], {
-              cancelable: true,
-            });
-          }
-        }}
-      />
-    </SafeAreaView>
+    <View style={{backgroundColor: '#6200ee'}}>
+      <TouchableOpacity
+        style={{flexDirection: 'row'}}
+        activeOpacity={0.4}
+        onPress={handleOnPress}>
+        <Image
+          source={require('../assets/logo_shw.jpg')}
+          style={{
+            margin: 4,
+            height: 35,
+            width: 25,
+            resizeMode: 'stretch',
+          }}
+        />
+        <Text style={{color: '#ffffff', fontSize: 18}}>Open Menu</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
