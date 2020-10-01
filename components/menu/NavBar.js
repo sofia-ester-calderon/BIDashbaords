434e03bd45e8bda9d6d93156078e3e61ec3614ca
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-10-01 16:48:49 +1000

    new menu layout

--- components/menu/NavBar.js
+++ components/menu/NavBar.js
@@ -3,7 +3,9 @@ import {Alert, Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import * as Navigation from './RootNavigation';
 import {useUserPermissions} from '../hooks/UserPermissionsProvider';
+
 const logoShw = require('../assets/logo_shw.jpg');
+const menu = require('../assets/menu_icon.png');
 
 const NavBar = () => {
   const [userPermissions] = useUserPermissions();
@@ -20,25 +22,33 @@ const NavBar = () => {
 
   return (
     <SafeAreaView style={{backgroundColor: '#6200ee'}}>
-      <TouchableOpacity
-        style={styles.HeaderStyle}
-        activeOpacity={0.4}
-        onPress={handleOnPress}>
+      <TouchableOpacity style={styles.HeaderStyle} activeOpacity={0.4} onPress={handleOnPress}>
+        <Image style={styles.MenuIconStyle} source={menu} />
         <Image source={logoShw} style={styles.ImageIconStyle} />
-        <Text style={styles.TextStyle}>Open Menu</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  HeaderStyle: {flexDirection: 'row'},
+  HeaderStyle: {
+    flexDirection: 'row',
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 15,
+    justifyContent: 'space-between',
+  },
 
   ImageIconStyle: {
-    margin: 4,
-    height: 25,
-    width: 18,
+    height: 35,
+    width: 25,
     resizeMode: 'stretch',
+  },
+  MenuIconStyle: {
+    height: 25,
+    width: 30,
+    resizeMode: 'stretch',
+    alignSelf: 'center',
   },
 
   TextStyle: {color: '#ffffff', fontSize: 18},
