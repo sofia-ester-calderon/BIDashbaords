9ebd2e959c5739a3bc81ae9c1561db53dd5cf50a
calderon <hugo.velasquez.le@gmail.com>
2020-10-02 13:40:09 +1000

    Added Home description

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,8 +1,44 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
+import {Text} from 'react-native-paper';
+const homeLogo = require('../assets/logo_shw.gif');
+
+const styles = StyleSheet.create({
+  logoIconStyle: {
+    margin: 20,
+    height: 250,
+    width: 200,
+    resizeMode: 'stretch',
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainText: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  subText: {
+    fontSize: 18,
+    textAlign: 'center',
+    fontStyle: 'italic',
+  },
+});
 
 const Home = () => {
-  return <Text>WELCOME</Text>;
+  return (
+    <View style={styles.layout}>
+      <Image style={styles.logoIconStyle} source={homeLogo} />
+      <Text style={styles.mainText}>
+        Soluciones {'\n'} Business Intelligence
+      </Text>
+      <Text style={styles.subText}>
+        "Optimizando los procesos empresariales"
+      </Text>
+    </View>
+  );
 };
 
 export default Home;
