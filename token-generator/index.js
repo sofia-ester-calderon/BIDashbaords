cb69b765df558d8fb099a1fa2681d2f84568d21b
Sofia Calderon <sofia.ester.calderon@gmail.com>
2020-10-08 18:20:02 +1100

    added script to generate tokens

---
+++ token-generator/index.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const key = require('./key.json');
+
+function generateToken(question) {
+  const payload = {
+    resource: {question},
+    params: {},
+  };
+  const token = jwt.sign(payload, key.value);
+  console.log(`TOKEN FOR QUESTION ${question}\n${token}\n\n`);
+}
+
+const question1 = process.argv[2];
+const question2 = process.argv[3];
+
+if (!question2) {
+  generateToken(question1);
+} else {
+  for (let question = question1; question <= question2; question++) {
+    generateToken(question);
+  }
+}
