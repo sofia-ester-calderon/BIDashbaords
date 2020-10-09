8bf91b7d97f9f7e262c44482fd4ff56644bd85db
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2020-10-08 18:37:17 -0600

    try useState

--- components/logout/LogoutContainer.js
+++ components/logout/LogoutContainer.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
 /* eslint-disable react/forbid-prop-types */
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {PropTypes} from 'prop-types';
 import {useFocusEffect} from '@react-navigation/native';
 import {Alert} from 'react-native';
@@ -12,7 +12,7 @@ import {useUserPermissions} from '../hooks/UserPermissionsProvider';
 
 const LogoutContainer = ({navigation}) => {
   const [userPermissions, userFunctions] = useUserPermissions();
-  let messages = {};
+  const [messages, setMessages] = useState({});
 
   const getMessages = () => {
     console.log('1 useEffect eingetreten');
@@ -22,10 +22,12 @@ const LogoutContainer = ({navigation}) => {
       .once('value')
       .then((logoutSnapshot) => {
         console.log('3 logoutSnapshot: ', logoutSnapshot.val());
-        messages = logoutSnapshot.val();
-        console.log('4 messages ist: ', messages);
-        console.log('5 und question ist: ', messages.question);
-        console.log('5 und no ist: ', messages.no);
+        const tmpMessages = logoutSnapshot.val();
+        console.log('4a tmpMessages ist: ', tmpMessages);
+        setMessages({...tmpMessages});
+        console.log('4b messages ist: ', messages);
+        console.log('5a und question ist: ', messages.question);
+        console.log('5b und no ist: ', messages.no);
       });
     console.log('6 Sprache ist: ', userPermissions.language);
     console.log('7 die Uebersetzungen sind: ', messages);
@@ -51,7 +53,7 @@ const LogoutContainer = ({navigation}) => {
         ],
         {cancelable: true},
       );
-      console.log('9 Nach Alert-Aufruf ', messages);
+      console.log('9 Messages nach Alert-Aufruf: ', messages);
 
       return () => {
         // Do something when the screen is unfocused
